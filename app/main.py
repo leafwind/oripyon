6ea759ac1c3480b15af2a5586cd5cec7bc555534
leafwind <leafwind.cs@gmail.com>
2017-02-11 11:38:58 -0500

    update phrase

--- app/main.py
+++ app/main.py
@@ -13,10 +13,8 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
-
-hourse_phrase = [
-'一馬當先', '千軍萬馬', '天馬行空', '心猿意馬', '犬馬之勞', '汗馬功勞', '老馬識途', '兵荒馬亂', '快馬加鞭', '求馬唐肆', '走馬上任', '走馬平川', '走馬看花', '車水馬龍', '招兵買馬', '金戈鐵馬', '青梅竹馬', '非驢非馬', '指鹿為馬', '害群之馬', '秣馬厲兵', '厲兵秣馬', '馬不停蹄', '馬耳東風', '馬到成功', '馬革裏屍', '馬首是瞻', '馬齒徒長', '單槍匹馬', '蛛絲馬跡', '塞翁失馬', '萬馬奔騰', '駟馬難追', '龍馬精神', '聲色犬馬', '懸崖勒馬', '露出馬腳', '牝牡驪黃'
-]
+from app.phrase import horse_phrase, lion_phrase, dunkey_phrase
+maple_phrase = horse_phrase + lion_phrase + dunkey_phrase
 
 application = Flask(__name__)
 
@@ -68,10 +66,10 @@ def handle_message(event):
 
 def make_reply(type, uid, msg):
     if '小路占卜'.decode('utf-8') in msg:
-        global hourse_phrase
+        global maple_phrase
         # profile = line_bot_api.get_profile(uid)
         random.seed(os.urandom(5))
-        ph = random.choice(hourse_phrase)
+        ph = random.choice(maple_phrase)
         return '今日運勢：{}'.decode('utf-8').format(ph.decode('utf-8'))
     #if '查'.decode('utf-8') in msg:
     #    profile = line_bot_api.get_profile(uid)
