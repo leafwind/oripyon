d986d96c892e6fa667d1cf365a9a23de31f6d8cb
leafwind <leafwind.cs@gmail.com>
2017-03-14 15:46:03 +0000

    refactor

--- app/main.py
+++ app/main.py
@@ -53,8 +53,11 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     print('=========')
-    print(event.__dict__)
-    print(event.source.__dict__)
+    print('timestamp: {}'.format(event.timestamp))
+    print('reply_token: {}'.format(event.reply_token))
+    print('type: {}'.format(event.type))
+    print('message: {}'.format(event.message.__dict__))
+    print('source: {}'.format(event.source.__dict__))
     leafwind_photo_url = 'https://static-cdn.jtvnw.net/jtv_user_pictures/panel-145336656-image-e9329cd5f8f44a76-320-320.png'
     kaori_photo_url = 'https://static-cdn.jtvnw.net/jtv_user_pictures/panel-145336656-image-4808e3743f50232e-320-320.jpeg'
     uri_action = make_template_action('uri', '前往卡歐的首頁', uri='http://yfting.com')
@@ -91,13 +94,15 @@ def handle_message(event):
             alt_text='buttons alt text', template=buttons_template)
         line_bot_api.reply_message(event.reply_token, template_message)
         return
-    elif event.source.type == 'room':
+    elif event.source.type == 'room':  # 自訂聊天
         rid = event.source.room_id
         reply = make_reply('room', rid, event.message.text)
     elif event.source.type == 'user':
         uid = event.source.user_id
         reply = make_reply('user', uid, event.message.text)
-    elif event.source.type == 'group':
+        # profile = line_bot_api.get_profile(uid)
+        # status = '{} 的顯圖：{}, 狀態：{}'.decode('utf-8').format(profile.display_name, profile.picture_url, profile.status_message)
+    elif event.source.type == 'group':  # 群組
         gid = event.source.group_id
         reply = make_reply('group', gid, event.message.text)
 
@@ -116,14 +121,9 @@ def make_reply(type, uid, msg):
         return cwb_weather_predictor.predict(msg_list[1].encode('utf-8').replace('台', '臺').decode('utf-8'))
     elif '小路占卜'.decode('utf-8') in msg:
         global maple_phrase
-        # profile = line_bot_api.get_profile(uid)
         random.seed(os.urandom(5))
         ph = random.choice(maple_phrase)
         return '今日運勢：{}'.decode('utf-8').format(ph.decode('utf-8'))
-    #if '查'.decode('utf-8') in msg:
-    #    profile = line_bot_api.get_profile(uid)
-    #    print(profile.__dict__)
-    #    return '{} 的顯圖：{}, 狀態：{}'.decode('utf-8').format(profile.display_name, profile.picture_url, profile.status_message)
     elif '幫QQ'.decode('utf-8') in msg:
         return '幫QQ喔'
     elif '魔法'.decode('utf-8') in msg:
