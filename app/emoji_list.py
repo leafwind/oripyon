3eb1f5c86d56a195e5da15a232226adb9fe0043a
connor hsu <connorhsu@appier.com>
2019-02-02 18:04:12 +0800

    update

--- app/emoji_list.py
+++ app/emoji_list.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 cry_emoji_list = [
     b'\xF0\x9F\x98\xA2',  # 😢
+    b'\xF0\x9F\x98\xAD',  # 😭
+    b'\xF0\x9F\x98\xBF',  # 😿
 ]
--- app/util.py
+++ app/util.py
@@ -5,7 +5,7 @@ import logging
 def get_short_url(target_url):
     test_access_token = '20f07f91f3303b2f66ab6f61698d977d69b83d64'
     api_url = 'https://api.pics.ee/v1/links/?access_token={}'.format(test_access_token)
-    r = requests.post(api_url, data = {"url": target_url})
+    r = requests.post(api_url, data={"url": target_url})
     if r.status_code == 200:
         return r.json()['data']['picseeUrl']
     else:
