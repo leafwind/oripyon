26e9320b3492ff5f3cfd1d91e9315655df306d6e
leafwind <leafwind.cs@gmail.com>
2019-02-02 09:59:21 +0000

    update

---
+++ app/emoji_list.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+cry_emoji_list = [
+    b'\xF0\x9F\x98\xA2',  # 😢
+]
---
+++ app/util.py
@@ -0,0 +1,12 @@
+import requests
+import logging
+
+
+def get_short_url(target_url):
+    test_access_token = '20f07f91f3303b2f66ab6f61698d977d69b83d64'
+    api_url = 'https://api.pics.ee/v1/links/?access_token={}'.format(test_access_token)
+    r = requests.post(api_url, data = {"url": target_url})
+    if r.status_code == 200:
+        return r.json()['data']['picseeUrl']
+    else:
+        logging.error(r.status_code)
