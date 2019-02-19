f11bc72a974b25c69e1032e7136e9c04ce8d679b
leafwind <leafwind.cs@gmail.com>
2019-02-19 06:14:31 +0000

    update

--- app/main.py
+++ app/main.py
@@ -115,6 +115,10 @@ def default(event):
                     elif event.message.sticker_id == '15335128':
                         reply = [TextSendMessage(text='EBB 不哭不哭出來嗨 ヽ(∀ﾟ )人( ﾟ∀)ﾉ')]
                         line_bot_api.reply_message(event.reply_token, reply)
+                if event.message.package_id == '1300920':
+                    if event.message.sticker_id == '12169612':
+                        reply = [TextSendMessage(text='EBB 不要躲出來嗨(換貼圖我還是會學到的！) ヽ(∀ﾟ )人( ﾟ∀)ﾉ')]
+                        line_bot_api.reply_message(event.reply_token, reply)
     else:
         raise ValueError
     logging.info(f"{GROUP_MAPPING.get(source_id, {'name': source_id}).get('name')}：{event.message}")
