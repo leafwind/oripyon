00ab6cad2aee53367dd9e8e33f446811b853f819
connor hsu <connorhsu@appier.com>
2019-02-22 15:49:17 +0800

    update

--- app/main.py
+++ app/main.py
@@ -29,7 +29,8 @@ application = Flask(__name__, template_folder='templates')
 user_info = {}
 user_info_map_file = os.path.join('line-user-info', 'users.json')
 if os.path.exists(user_info_map_file):
-    user_info = json.load(user_info_map_file)
+    with open(user_info_map_file, 'r') as f:
+        user_info = json.load(user_info_map_file)
 
 with open("line_auth_key.yml", 'r') as stream:
     data = yaml.load(stream)
