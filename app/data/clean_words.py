7cb7a32a1dfa10e71c464eeb88f64b80d5460cc1
connor hsu <connorhsu@appier.com>
2019-02-23 20:06:54 +0800

    update

--- app/data/clean_words.py
+++ app/data/clean_words.py
@@ -4,13 +4,18 @@ with open('dict.txt.zhtw.sort') as f:
     data = f.readlines()
     with open('dict.txt.zhtw.trimsize', 'w') as f_out:
         for row in data:
-            word = row.split(' ')[0]
+            _ = row.split(' ')
+            word = _[0]
+            freq = int(_[1])
             if len(word) == 1:
                 skip_stats[len(word)] += 1
                 print(f'skip {word}')
                 continue
             elif len(word) > 5:
                 skip_stats[len(word)] += 1
+                print(f'skip {word}')
+                continue
+            elif freq < 10:
                 print(f'skip {word}')
                 continue
             else:
