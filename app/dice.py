8c2a7c3d3a87408712df942b8e39a08ccda8597c
connor hsu <connorhsu@appier.com>
2019-03-24 18:16:03 +0800

    update

--- app/dice.py
+++ app/dice.py
@@ -16,8 +16,9 @@ def draw_cat():
     sh = gss_client.open_by_key(GSPREAD_KEY_CAT)
     worksheet = sh.get_worksheet(0)
     list_of_lists = worksheet.get_all_values()
+    logging.info(list_of_lists)
     row = random.choice(list_of_lists)
-    replies = [('image', row[0]), ('text', row[1])]
+    replies = [('image', row[0]), ('text', f'{row[1]}：{row[2]}')]
     return replies
 
 
