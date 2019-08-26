3f084e1137b7305d0ad5b028109b0fe3d303fe0e
connor <connor@mbp-schumi-grey.local>
2019-08-26 23:42:09 +0800

    add message generator

---
+++ message_generator/flex_message_generator.py
@@ -0,0 +1,32 @@
+from linebot.models import (
+    MessageAction, BubbleContainer, BoxComponent,
+    ButtonComponent, TextComponent, SeparatorComponent
+)
+
+
+def build_top_menu_function_card_content(title, text_contents):
+    contents = [
+        ButtonComponent(
+            style='primary',
+            height='sm',
+            color='#95B9B4',
+            action=MessageAction(label=label, text=text),
+        )
+        for (label, text) in text_contents
+    ]
+    container = BubbleContainer(
+        direction='ltr',
+        body=BoxComponent(
+            layout='vertical',
+            contents=[
+                TextComponent(text=title, weight='bold', size='xl'),
+                SeparatorComponent(),
+                BoxComponent(
+                    layout='horizontal',
+                    spacing='sm',
+                    contents=contents
+                )
+            ],
+        ),
+    )
+    return container
