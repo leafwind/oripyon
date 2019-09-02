8ac3f2ec9c7309f0a01595b63d769356e0bfb3cd
connor <leafwind.cs@gmail.com>
2019-09-02 23:28:15 +0800

    update

--- app/private_reply.py
+++ app/private_reply.py
@@ -8,72 +8,95 @@ from linebot.models import (
 
 
 def build_top_menu_function_card_message_tool(_msg_info, _robot_settings):
-    title = '工具類別指令範例'
+    title = '工具'
     text_content = [
         [
-            ('水庫(beta)', '水庫'),
-            ('匯率(beta)', '匯率'),
+            ('水庫', '水庫'),
         ],
+        [
+            ('匯率', '匯率'),
+        ]
     ]
     bubble1 = flex_message_generator.build_top_menu_function_card_content(title=title, text_contents=text_content,
-                                                                          bubble_size='nano')
-    title = '氣象指令(氣象局圖檔)'
+                                                                          bubble_size='micro')
+    title = '氣象圖'
     text_content = [
         [
             ('天氣', '天氣'),
+        ],
+        [
             ('即時雨量', '即時雨量'),
         ],
         [
             ('雷達', '雷達'),
-            ('空品', '空品'),
         ],
+        [
+            ('空品', '空品'),
+        ]
     ]
     bubble2 = flex_message_generator.build_top_menu_function_card_content(title=title, text_contents=text_content,
-                                                                          bubble_size='nano')
+                                                                          bubble_size='micro')
     carousel_container = CarouselContainer([bubble1, bubble2])
     return [('flex', FlexSendMessage(alt_text='工具類別指令範例', contents=carousel_container))]
 
 
 def build_top_menu_function_card_message_dice(_msg_info, _robot_settings):
-    title = '運勢類別指令範例'
+    title = '運勢？'
     text_content = [
         [
             ('運勢', '運勢 考試'),
             ('塔羅', '塔羅 上班'),
         ],
+    ]
+    bubble1 = flex_message_generator.build_top_menu_function_card_content(title=title, text_contents=text_content,
+                                                                          bubble_size='nano')
+    title = '占卜？'
+    text_content = [
         [
-            ('請問為什麼', '請問為什麼會下雨'),
-            ('何老師抽卡', '何老師抽卡'),
+            ('為什麼', '請問為什麼會下雨'),
+            ('何老師', '何老師抽卡'),
         ],
     ]
-    bubble1 = flex_message_generator.build_top_menu_function_card_content(title=title, text_contents=text_content)
-
-    title = '骰子類別指令範例'
+    bubble2 = flex_message_generator.build_top_menu_function_card_content(title=title, text_contents=text_content,
+                                                                          bubble_size='nano')
+    title = '骰子？'
     text_content = [
-        [('choice[x,y,z]', 'choice[排骨飯,豬腳飯,焢肉飯]')],
-        [('NCA [隨意] (攻擊檢定)', 'NCA 攻擊小明')],
-        [('cc(z)<=x (COC 7e)', 'cc<=50')],
+        [('choice', 'choice[排骨飯,豬腳飯,焢肉飯]')],
+        [('NCA', 'NCA 攻擊小明')],
+        [('cc<x', 'cc<=50')],
     ]
-    bubble2 = flex_message_generator.build_top_menu_function_card_content(title=title, text_contents=text_content)
-    carousel_container = CarouselContainer([bubble1, bubble2])
+    bubble3 = flex_message_generator.build_top_menu_function_card_content(title=title, text_contents=text_content,
+                                                                          bubble_size='nano')
+    carousel_container = CarouselContainer([bubble1, bubble2, bubble3])
     return [('flex', FlexSendMessage(alt_text='運勢類別指令範例', contents=carousel_container))]
 
 
 def build_top_menu_function_card_message_others(_msg_info, _robot_settings):
-    title = '彩蛋類別指令範例'
+    title = '莫名？'
     text_content = [
         [
             ('幫QQ', '幫QQ'),
         ],
         [
             ('爛中文', '爛中文'),
-        ],
+        ]
+    ]
+    bubble1 = flex_message_generator.build_top_menu_function_card_content(title=title, text_contents=text_content,
+                                                                          bubble_size='nano')
+
+    title = '魔法？'
+    text_content = [
         [
             ('咕嚕靈波', '咕嚕靈波'),
         ],
         [
             ('魔法少女', '魔法少女'),
         ],
+    ]
+    bubble2 = flex_message_generator.build_top_menu_function_card_content(title=title, text_contents=text_content,
+                                                                          bubble_size='nano')
+    title = '還願？'
+    text_content = [
         [
             ('慈孤觀音', '慈孤觀音'),
         ],
@@ -81,9 +104,12 @@ def build_top_menu_function_card_message_others(_msg_info, _robot_settings):
             ('女兒紅', '女兒紅'),
         ]
     ]
-    container = flex_message_generator.build_top_menu_function_card_content(title=title, text_contents=text_content,
-                                                                            bubble_size='nano')
-    return [('flex', FlexSendMessage(alt_text='彩蛋類別指令範例', contents=container))]
+    bubble3 = flex_message_generator.build_top_menu_function_card_content(title=title, text_contents=text_content,
+                                                                          bubble_size='nano')
+
+    carousel_container = CarouselContainer([bubble1, bubble2, bubble3])
+
+    return [('flex', FlexSendMessage(alt_text='彩蛋類別指令範例', contents=carousel_container))]
 
 
 pattern_mapping_private = [
