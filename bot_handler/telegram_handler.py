f1c39b278031dcd1014301fb63c7ec2332e39f16
connor <leafwind.cs@gmail.com>
2020-02-24 11:23:55 +0900

    update

--- bot_handler/telegram_handler.py
+++ bot_handler/telegram_handler.py
@@ -2,27 +2,6 @@ import json
 import logging
 
 
-def handle_message(telegram_bot, message):
-    if message.text is None:
-        return
-    text = message.text
-    if '/echo' in text:
-        echo(telegram_bot, message)
-
-
-def echo(telegram_bot, message):
-    """
-    repeat the same message back (echo)
-    """
-    _cmd, text = parse_cmd_text(message.text)
-    if text is None or len(text) == 0:
-        pass
-    else:
-        logging.info(f'message.chat.id: {message.chat.id}, text: {text}')
-        chat_id = message.chat.id
-        telegram_bot.sendMessage(chat_id=chat_id, text=text.decode("utf-8"))
-
-
 def parse_cmd_text(text):
     # Telegram understands UTF-8, so encode text for unicode compatibility
     text = text.encode('utf-8')
@@ -35,3 +14,24 @@ def parse_cmd_text(text):
         cmd = text[:i]
         text = text[i + 1:]
     return cmd, text
+
+
+def echo(telegram_bot, message):
+    """
+    repeat the same message back (echo)
+    """
+    _cmd, text = parse_cmd_text(message.text)
+    if text is None or len(text) == 0:
+        pass
+    else:
+        logging.info(f'message.chat.id: {message.chat.id}, text: {text}')
+        chat_id = message.chat.id
+        telegram_bot.sendMessage(chat_id=chat_id, text=text)
+
+
+def handle_message(telegram_bot, message):
+    if message.text is None:
+        return
+    text = message.text
+    if '/echo' in text:
+        echo(telegram_bot, message)
