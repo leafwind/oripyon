174583f1e8ddfc54a177fa972fe893f3f3ed7556
connor <leafwind.cs@gmail.com>
2020-02-29 00:36:00 +0900

    u

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -7,5 +7,5 @@ workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'gevent'
 certfile = '/etc/ssl/private/letsencrypt-domain.pem'
 keyfile = '/etc/ssl/private/letsencrypt-domain.key'
-accesslog = '-'  # stdout
+accesslog = '/var/log/gunicorn/access.log'
 errorlog = '-'  # stderr
