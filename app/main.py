de230bb6bd45f55dd1ec48236402ad8af9ae3d08
connor <leafwind.cs@gmail.com>
2020-03-02 09:36:32 +0900

    u

--- app/main.py
+++ app/main.py
@@ -96,7 +96,8 @@ def telegram_callback():
             lat = location.latitude
             lon = location.longitude
             geo_info = reverse_geocode_customize((lat, lon))[0]
-            update.message.reply_text(f'您的位置資訊：{geo_info["name"]}, {geo_info["admin1"]}, {geo_info["cc"]} ({lat}, {lon})')
+            user = update.effective_user
+            update.message.reply_text(f'{user.fullname}({user.id})您的位置資訊：{geo_info["name"]}, {geo_info["admin1"]}, {geo_info["cc"]} ({lat}, {lon})')
     return 'OK'
 
 
