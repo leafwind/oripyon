edbd9e33a4ba6bf14a46e76e29271e28a3ddfc67
connor <leafwind.cs@gmail.com>
2021-01-24 18:44:19 +0900

    chore(): update

--- bot_handler/tg_likecoin.py
+++ bot_handler/tg_likecoin.py
@@ -5,10 +5,12 @@ import logging
 
 import requests
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
-from telegram.ext import MessageHandler, Filters, CommandHandler, CallbackContext
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import MessageHandler, Filters, CommandHandler, CallbackContext, CallbackQueryHandler
 
 reply_rabbit_icon = "\U0001F430"
 validator_icon = "\U0001F47E"
+validators = []
 
 
 def get_function_keyboard_markup(chat_type):
@@ -29,16 +31,24 @@ def get_function_keyboard_markup(chat_type):
     return markup
 
 
+def get_single_validator_status(update: Update, _context: CallbackContext):
+    return
+
+
 def proposal_status(update: Update, _context: CallbackContext):
     update.message.reply_text("TODO")
 
 
 def validator_status(update: Update, _context: CallbackContext):
+    global validators
     r = requests.get("https://mainnet-node.like.co/staking/validators")
     result = r.json()
     validators = result["result"]
     validator_buttons = [
-        KeyboardButton(text=f"{validator_icon} {v['description']['moniker']}")
+        InlineKeyboardButton(
+            text=v['description']['moniker'],
+            callback_data=f"{validator_icon} {v['operator_address']}"
+        )
         for v in validators
     ]
     validator_table = []
@@ -49,14 +59,7 @@ def validator_status(update: Update, _context: CallbackContext):
         for i in range(num_of_col):
             row.append(validator_buttons.pop(0))
         validator_table.append(row)
-    final_row = [KeyboardButton(text='\U0000274E 關閉鍵盤')]
-    validator_table.append(final_row)
-
-    markup = ReplyKeyboardMarkup(
-        validator_table,
-        resize_keyboard=True,
-        selective=True,
-    )
+    markup = InlineKeyboardMarkup(validator_table)
 
     reply = f"請選擇要查詢的驗證人 {reply_rabbit_icon}"
     update.message.reply_text(
@@ -77,28 +80,45 @@ def close_keyboard(update: Update, _context: CallbackContext):
     update.message.reply_text(reply, reply_markup=ReplyKeyboardRemove())
 
 
-def make_reply(update: Update, _context: CallbackContext):
+def text_reply_handler(update: Update):
     # _args = context.args
     if not update.message.text:
         logging.warning(f'no text attribute in: {update.message}')
     text = update.message.text
-    logger = logging.getLogger(__name__)
     if text == "\U0001F5F3 現有議案":
-        proposal_status(update, _context)
+        proposal_status(update)
     elif text == f"{validator_icon} 驗證人狀態":
-        validator_status(update, _context)
+        validator_status(update)
     elif text == "\U0001F465 建議交流":
-        feedback(update, _context)
+        feedback(update)
     elif text == "\U0000274E 關閉鍵盤":
-        close_keyboard(update, _context)
-    elif "沒事了" in text:
-        reply = f"沒事就好 {reply_rabbit_icon}"
-        logger.info(f"reply: {reply}")
-        update.message.reply_text(reply)
+        close_keyboard(update)
+    elif text.startswith(f"{validator_icon} "):
+        get_single_validator_status(update)
 
 
-def bot_help(update: Update, _context: CallbackContext):
-    # _args = context.args
+def wrap_code_block(text: str) -> str:
+    return "```\n" + text + "\n```"
+
+
+def callback_query_handler(bot, update):
+    cqd = update.callback_query.data
+    if cqd.startswith(validator_icon):
+        validator_address = cqd.split()[1]
+        logging.info(f"validator_address: {validator_address}")
+        for v in validators:
+            if validator_address == v['validator_address']:
+                commission_rate = v["commission"]["commission_rates"]["rate"]
+                text = f"validator: {v['description']['moniker']}" \
+                    f"commission rate: {commission_rate}"
+                bot.send_message(
+                    chat_id=update.message.from_user.id,
+                    text=wrap_code_block(text),
+                    parse_mode="MarkdownV2",
+                )
+
+
+def start_handler(update: Update):
     update.message.reply_text(
         text=f'你可以：\n'
              f'輸入 "/" 展開指令選單\n'
@@ -109,5 +129,6 @@ def bot_help(update: Update, _context: CallbackContext):
 
 
 def add_handlers(dispatcher):
-    dispatcher.add_handler(CommandHandler("help", bot_help, pass_args=True))
-    dispatcher.add_handler(MessageHandler(Filters.text, make_reply))
+    dispatcher.add_handler(CommandHandler("start", start_handler))
+    dispatcher.add_handler(MessageHandler(Filters.text, text_reply_handler))
+    dispatcher.add_handler(CallbackQueryHandler(callback_query_handler))
